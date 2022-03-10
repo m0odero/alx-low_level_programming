3f9562bba248984a95d5db85730779f6855e76d1
m0odero <margaretodero59@gmail.com>
2022-03-10 10:31:47 -0800

    use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a string
+ * Return: 0 of there is no error
+ */
+
+int main(void)
+{
+	str[] = "with proper grammar, but the outcome is a piece of art,\n";
+	printf(str);
+	return (0);
+}
