77d6950c4f775a59363710a9c8efb9bd06c7d8fe
m0odero <margaretodero59@gmail.com>
2022-03-11 06:03:43 -0800

    put char except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase followed by a newline
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
