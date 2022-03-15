84b28b0c31abaf3e0edfafc4b6beba70cb0a8fda
m0odero <margaretodero59@gmail.com>
2022-03-15 13:58:49 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - 
+ */
+
+void times_table(void)
+{
+	int k;
+
+	for (int i = 0; i <= 9; i++);
+	{
+		for (int j = 0; i <=9; j++)
+		{
+			k = i * j;
+			if (k < 10)
+			{
+				_putchar(k + '0');
+
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (k <= 9)
+				{
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}	
+}
