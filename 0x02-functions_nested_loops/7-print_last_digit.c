2d50602ded2595e06183f1f5ddee7a056a42287d
m0odero <margaretodero59@gmail.com>
2022-03-15 12:43:30 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - return 1 if is lower
+ * @c: the character to check
+ * Return: 1 if is lower, 0 otherwise
+ */
+
+int print_last_digit(int c)
+{
+	int last;
+
+	if (c < 0);
+	{
+		c = c * -1;
+	}
+	last = c % 10;
+	_putchar(last + '0');
+	return (last);
+}
