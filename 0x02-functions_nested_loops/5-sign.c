c570eb7a615b378e327574c0abda33dd9d954ecb
m0odero <margaretodero59@gmail.com>
2022-03-15 12:07:58 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - return 1 if is lower
+ * @c: the character to check
+ * Return: 1 if is lower, 0 otherwise
+ */
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		if (c == 0)
+		{
+			_putchar(0);
+			return (0);
+		}
+		else
+		{
+			_putchar('-');
+			return (-1);
+		}
+	}
+}
