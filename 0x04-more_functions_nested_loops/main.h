90f7fa94e27431e3078c7a0567c7b3aea32cef45
m0odero <margaretodero59@gmail.com>
2022-03-17 11:10:58 -0700

    multiply

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
