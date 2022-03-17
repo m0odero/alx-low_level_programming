4ddd1c50ff7e63e17178f25c38d2d4d06bb6b6d0
m0odero <margaretodero59@gmail.com>
2022-03-17 11:45:10 -0700

    print numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,8 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
