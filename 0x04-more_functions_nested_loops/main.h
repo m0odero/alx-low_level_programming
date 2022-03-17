ff09e98dbf073a98a68a6f24430d256e3d655842
m0odero <margaretodero59@gmail.com>
2022-03-17 12:30:40 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,6 +11,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
