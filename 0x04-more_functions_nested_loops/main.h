56f4a26ab2f474d44feb983f820ae13e54604a45
m0odero <margaretodero59@gmail.com>
2022-03-17 12:33:22 -0700

    print square

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,6 +12,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
