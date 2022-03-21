2e1b788960347dd5b94efcd60f954c8be52fb42c
m0odero <margaretodero59@gmail.com>
2022-03-21 03:51:02 -0700

    fixed some errors

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -20,12 +20,26 @@ void rev_string(char *s)
 	i--;
 	len = i;
 	j = 0;
-	while (i  >= len / 2)
+	if (len % 2 == 0)
 	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i--;
-		j++;
+		while (i  >= len / 2)
+		{
+			temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+			i--;
+			j++;
+		}
+	}
+	else
+	{
+		while (i >= (len + 1) / 2)
+		{
+			temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+			i--;
+			j++;
+		}
 	}
 }
